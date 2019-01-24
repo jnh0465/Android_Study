a7caa52ab32977af529d51fedb284427248869b4
jiwoolee <jnh0465@naver.com>
2019-01-24 19:11:00 +0900

    feat : Access DB using ContentValues and SQLiteOpenHelper in DBActivity

--- Android_back/app/src/main/java/com/jiwoolee/android_back/DataManagement/DBActivity.java
+++ Android_back/app/src/main/java/com/jiwoolee/android_back/DataManagement/DBActivity.java
@@ -1,6 +1,7 @@
 package com.jiwoolee.android_back.DataManagement;
 
 import android.Manifest;
+import android.content.ContentValues;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -24,8 +25,10 @@ import java.util.Locale;
 
 public class DBActivity extends AppCompatActivity {
     //SQLite = 안드로이드 내장 DB, MySQL과 유사
+    // 1) 표준 SQL 쿼리문 사용
+    // 2) 클래스 이용
 
-    TextView text40;
+    TextView text40, text43;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +36,7 @@ public class DBActivity extends AppCompatActivity {
         setContentView(R.layout.activity_db);
 
         text40 = (TextView)findViewById(R.id.textView40);
+        text43 = (TextView)findViewById(R.id.textView43);
     }
 
     public void btn_insert(View view){
@@ -52,6 +56,33 @@ public class DBActivity extends AppCompatActivity {
         db.close();
 
         text40.setText("DB저장완료");
+    }
+
+    public void btn_insert_class(View view){
+        DBHelper2 helper = new DBHelper2(this);
+        SQLiteDatabase db = helper.getWritableDatabase();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()); //2019-01-15
+        String date = sdf.format(new Date());
+
+        //String sql = "insert into TestTable (textData, intData, floatData, dateData) values (?, ?, ?, ?)";
+        ContentValues cv1 = new ContentValues();
+        cv1.put("textData", "insert_class1"); //컬렴명/데이터값 지정
+        cv1.put("intData", 100);
+        cv1.put("floatData", 11.11);
+        cv1.put("dateData", date);
+
+        ContentValues cv2 = new ContentValues();
+        cv2.put("textData", "insert_class2");
+        cv2.put("intData", 200);
+        cv2.put("floatData", 22.22);
+        cv2.put("dateData", date);
+
+        db.insert("TestTable2", null, cv1); //테이블명 지정
+        db.insert("TestTable2", null, cv2);
+        db.close();
+
+        text43.setText("DB저장완료");
     }
 
     public void btn_DB(View view){
@@ -89,6 +120,36 @@ public class DBActivity extends AppCompatActivity {
         db.close();
     }
 
+    public void btn_DB_class(View view){
+        DBHelper2 helper = new DBHelper2(this);
+        SQLiteDatabase db = helper.getWritableDatabase();
+        text43.setText("");
+
+        //select 컬럼명들 from 테이블명 where 조건절 group by 기준컬럼 having 조건절 order by 컬럼명
+        Cursor c = db.query("TestTable2", null, null, null, null, null, null);
+
+        while(c.moveToNext()){ //1->2->3->4->
+            int idx_pos = c.getColumnIndex("idx"); // 컬럼의 인덱스 번호를 추출
+            int textData_pos = c.getColumnIndex("textData");
+            int intData_pos = c.getColumnIndex("intData");
+            int floatData_pos = c.getColumnIndex("floatData");
+            int dateData_pos = c.getColumnIndex("dateData");
+
+            int idx = c.getInt(idx_pos); // 컬럼 인덱스 번호를 통해 데이터를 가져옴
+            String textData = c.getString(textData_pos);
+            int intData = c.getInt(intData_pos);
+            double floatData = c.getDouble(floatData_pos);
+            String dateData = c.getString(dateData_pos);
+
+            text43.append("idx : " + idx + "\n");
+            text43.append("textData : " + textData + "\n");
+            text43.append("intData : " + intData + "\n");
+            text43.append("floatData : " + floatData + "\n");
+            text43.append("dateData : " + dateData + "\n\n");
+        }
+        db.close();
+    }
+
     public void btn_update(View view){
         DBHelper helper = new DBHelper(this);
         SQLiteDatabase db = helper.getWritableDatabase();
@@ -102,6 +163,21 @@ public class DBActivity extends AppCompatActivity {
         text40.setText("수정완료");
     }
 
+    public void btn_update_class(View view){
+        DBHelper2 helper = new DBHelper2(this);
+        SQLiteDatabase db = helper.getWritableDatabase();
+
+        //String sql = "update TestTable set textData = ? where idx = ?";
+        ContentValues cv = new ContentValues();
+        cv.put("textData", "update");
+        String where = "idx=?"; //조건절
+        String [] args = {"1"};
+        db.update("TestTable2", cv, where, args);
+        db.close();
+
+        text43.setText("수정완료");
+    }
+
     public void btn_delete(View view){
         DBHelper helper = new DBHelper(this);
         SQLiteDatabase db = helper.getWritableDatabase();
@@ -113,5 +189,19 @@ public class DBActivity extends AppCompatActivity {
         db.close();
 
         text40.setText("삭제 완료");
+    }
+
+    public void btn_delete_class(View view){
+        DBHelper2 helper = new DBHelper2(this);
+        SQLiteDatabase db = helper.getWritableDatabase();
+
+        //String sql = "delete from TestTable where idx=?";
+        String where = "idx=?"; //조건절
+        String [] args = {"2"};
+
+        db.delete("TestTable2", where, args);
+        db.close();
+
+        text43.setText("삭제 완료");
     }
 }
---
+++ Android_back/app/src/main/java/com/jiwoolee/android_back/DataManagement/DBHelper2.java
@@ -0,0 +1,42 @@
+package com.jiwoolee.android_back.DataManagement;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+//SQLite 연결
+public class DBHelper2 extends SQLiteOpenHelper {
+    public DBHelper2(Context context){
+        super(context, "Test2.db", null, 1);
+        //super(context, "Test.db", null, 2);
+        //DB 이름                 구조변경시 버전(테이블2->3개)
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) { //Test.db 여부 확인, 없을시 생성하고 다시 호출 / 테이블 생성, 기타
+        //Log.d("test", "DB 생성");
+        String sql = "create table TestTable2("
+                + "idx integer primary key autoincrement, "
+                + "textData text not null, "
+                + "intData integer not null, "
+                + "floatData real not null, "
+                + "dateData date not null"
+                + ")";
+
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //Log.d("test", "old : "+oldVersion);
+        //Log.d("test", "new : "+newVersion);
+        switch (oldVersion){
+            case 1 :
+                // 1에서 2 버전 형태로 테이블 구조를 변경시키는 작업을 한다.
+            case 2 :
+                // 2에서 3버전 형태로 테이블 구조를 변경시키는 작업을 한다.
+            case 3 :
+                // 3에서 4버전 형태로 테이블 구조를 변경시키는 작업을 한다.
+        }
+    }
+}
